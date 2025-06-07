7004f8b8f9d10239ff547f75b4da1f07404880b4
OmarHamd11 <omarellafy1@gmail.com>
2025-06-08 02:47:09 +0300

    ending deploy model

--- utils.py
+++ utils.py
@@ -1,19 +1,10 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 import numpy as np
+import io
 
-# Load the model once
-model = load_model("model/cnn_model.h5")
-
-# Define your class labels (update as per your model)
-class_labels = ['Aloe Vera', 'Basil', 'Mint', 'Rosemary', 'Snake Plant']
-
-def predict_plant(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))  # Change if your model uses another size
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize if you did during training
-
-    prediction = model.predict(img_array)
-    predicted_class = np.argmax(prediction[0])
-    return class_labels[predicted_class]
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    image = image.resize((256, 256))
+    #image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
